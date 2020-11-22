70545b2697dc81410a97add57579557c60dfe958
Nicolas Harraudeau <nharraud@users.noreply.github.com>
2020-11-22 23:10:37 +0100

    Remove Book and Cube code.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Profiler } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Books } from './Books';
 import GameCanvas from './GameCanvas';
 
 import { ApolloProvider } from '@apollo/client';
@@ -12,10 +10,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
     <div className="App" style={{height:"100%",width:"100%"}}>
-    {/* <Profiler id="Books" onRender={(...props)=> {console.log(props);}}> */}
-      {/* <Books/> */}
       <GameCanvas/>
-      {/* </Profiler> */}
     </div>
     </ApolloProvider>
   );
