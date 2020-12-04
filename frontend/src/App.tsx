9265d79e1922bc852ef64439d5ac3534f4b952f3
Nicolas Harraudeau <nharraud@users.noreply.github.com>
2020-12-04 22:31:42 +0100

    Start UI with material-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,46 @@
 import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Container from '@material-ui/core/Container';
+
 import logo from './logo.svg';
 import './App.css';
 import GameCanvas from './GameCanvas';
+import Login from './ui/Login';
+import Registration from './ui/Registration';
 
 import { ApolloProvider } from '@apollo/client';
 import { client } from './apolloConfig';
+
+import MenuBar from './ui/MenuBar';
 
 function App() {
   return (
     <ApolloProvider client={client}>
     <div className="App" style={{height:"100%",width:"100%"}}>
-      <GameCanvas/>
+      <Router>
+      <MenuBar/>
+
+      <Switch>
+        <Route path="/game">
+          <GameCanvas/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/register">
+          <Registration/>
+        </Route>
+      </Switch>
+
+      
+      </Router>
     </div>
     </ApolloProvider>
   );
---
+++ frontend/src/ui/Login.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      // flexGrow: 1,
+    },
+    left: {
+      backgroundColor: 'black'
+    },
+    formBox: {
+      marginTop: theme.spacing(10),
+    },
+    form: {
+      padding: theme.spacing(5),
+      textAlign: 'left'
+    },
+    formGrid: {
+      margin: 'auto'
+    },
+    formTitle: {
+      paddingBottom: theme.spacing(3),
+      textAlign: 'center'
+    },
+    formButtons: {
+      marginTop: theme.spacing(2)
+    }
+  }),
+);
+
+export default function Login(props) {
+  const classes = useStyles();
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submitForm = event => {
+    event.preventDefault()
+
+    const options = {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      body: `email=${email}&password=${password}`
+    }
+  }
+
+
+  return (
+    <div className={classes.root}>
+      <Grid container xs={12} spacing={3} direction="row" justify="center">
+        <Grid item xs={4}>
+          <Paper className={classes.formBox} elevation={3}>
+            <form className={classes.form} noValidate autoComplete="off">
+              <Typography className={classes.formTitle} variant="h4" component="h2" gutterBottom>
+                Login
+              </Typography>
+              <Grid className={classes.formGrid} container xs={12} spacing={3} direction="row" justify="center">
+                <Grid item xs={12}>
+                  <TextField label="username" variant="outlined" fullWidth />
+                </Grid>
+                <Grid item xs={12} lg={12}>
+                  <TextField label="password" variant="outlined" fullWidth />
+                </Grid>
+                <Grid item container xs={12} className={classes.formButtons} justify="center">
+                  <Button variant="contained" color="primary">
+                    login
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
