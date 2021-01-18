cc4a38ab87f0a6d3bfd35e4a8b27238ff3731434
Nicolas Harraudeau <nharraud@users.noreply.github.com>
2021-01-18 22:36:50 +0100

    Improve eslint configuration

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,14 +3,37 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
+
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
   rules: {
     'prettier/prettier': ['error'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
 };
