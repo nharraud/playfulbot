b3097a6fee45672aa7bdc50e22e6036386d2041a
Nicolas Harraudeau <nharraud@users.noreply.github.com>
2021-06-08 17:04:54 +0200

    Refactor GameVersionSlider

--- frontend/src/ui/Tournament/DebugDrawer/DebugGameTab.tsx
+++ frontend/src/ui/Tournament/DebugDrawer/DebugGameTab.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { Game } from 'src/types/graphql-generated';
 import { ControlledGame, SetGameVersion } from 'src/hooksAndQueries/useGameController';
-import { Slider, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import GameVersionSlider from './GameVersionSlider';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -12,59 +13,20 @@ const useStyles = makeStyles((theme) => ({
     paddingRight: theme.spacing(10),
     paddingLeft: theme.spacing(10),
   },
-  versionSlider: {},
   waitMessage: {
     padding: theme.spacing(2),
   }
 }));
 
-interface DevbugGameTabProps {
+interface DebugGameTabProps {
   game: Game,
   createDebugGame: () => void,
   controlledGame: ControlledGame,
   setGameVersion: SetGameVersion,
 }
 
-export default function DebugGameTab({ game, createDebugGame, controlledGame, setGameVersion}: DevbugGameTabProps) {
+export default function DebugGameTab({ game, createDebugGame, controlledGame, setGameVersion}: DebugGameTabProps) {
   const classes = useStyles();
-
-  const [slidingVersion, setSlidingVersion] = useState<number>(undefined);
-  const [changingVersion, setChangingVersion] = useState<boolean>(false);
-  const [maxVersion, setMaxVersion] = useState<number>(0);
-
-  const onSlidingVersionCommit = useCallback((event, version) => {
-    setChangingVersion(false);
-    setGameVersion(version);
-  }, [setGameVersion]);
-
-  const onSlidingVersionChange = useCallback((event, version) => {
-    setChangingVersion(true);
-    setSlidingVersion(version);
-  }, [setSlidingVersion]);
-
-  useEffect(() => {
-    if (controlledGame !== undefined && slidingVersion !== controlledGame.version && !changingVersion) {
-      setSlidingVersion(controlledGame.version);
-    }
-    if (controlledGame !== undefined && maxVersion !== controlledGame.maxVersion) {
-      setMaxVersion(controlledGame.maxVersion)
-    }
-  }, [
-    controlledGame, controlledGame?.version,
-    slidingVersion, changingVersion, setSlidingVersion,
-    maxVersion, setMaxVersion
-  ])
-
-  const marks = [
-    {
-      value: 0,
-      label: '0',
-    },
-    {
-      value: game?.version || 0,
-      label: (game?.version || 0).toString(),
-    },
-  ];
 
   let slider;
   let waitMessage;
@@ -74,17 +36,9 @@ export default function DebugGameTab({ game, createDebugGame, controlledGame, se
         <Typography id='game-version-slider-title' variant="h6" gutterBottom>
           Game Turns
         </Typography>
-        <Slider className={classes.versionSlider}
-          value={slidingVersion === undefined ? 0 : slidingVersion}
-          onChangeCommitted={onSlidingVersionCommit}
-          onChange={onSlidingVersionChange}
-          aria-labelledby="game-version-slider-title"
-          step={1}
-          marks={marks}
-          min={0}
-          max={maxVersion}
-          color="secondary"
-          valueLabelDisplay={ "on" }
+        <GameVersionSlider
+          controlledGame={controlledGame}
+          setGameVersion={setGameVersion}
         />
       </div>
     )
@@ -106,5 +60,3 @@ export default function DebugGameTab({ game, createDebugGame, controlledGame, se
     </div>
   );
 }
-
-// export default DebugGameTab;
---
+++ frontend/src/ui/Tournament/DebugDrawer/GameVersionSlider.tsx
@@ -0,0 +1,70 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { ControlledGame, SetGameVersion } from 'src/hooksAndQueries/useGameController';
+import { Slider } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  versionSlider: {}
+}));
+
+interface GameVersionSliderProps {
+  controlledGame: ControlledGame,
+  setGameVersion: SetGameVersion,
+}
+
+export default function GameVersionSlider({ controlledGame, setGameVersion}: GameVersionSliderProps) {
+  const classes = useStyles();
+
+  const [slidingVersion, setSlidingVersion] = useState<number>(undefined);
+  const [changingVersion, setChangingVersion] = useState<boolean>(false);
+  const [maxVersion, setMaxVersion] = useState<number>(0);
+
+  const onSlidingVersionCommit = useCallback((event, version) => {
+    setChangingVersion(false);
+    setGameVersion(version);
+  }, [setGameVersion]);
+
+  const onSlidingVersionChange = useCallback((event, version) => {
+    setChangingVersion(true);
+    setSlidingVersion(version);
+  }, [setSlidingVersion]);
+
+  useEffect(() => {
+    if (controlledGame !== undefined && slidingVersion !== controlledGame.version && !changingVersion) {
+      setSlidingVersion(controlledGame.version);
+    }
+    if (controlledGame !== undefined && maxVersion !== controlledGame.maxVersion) {
+      setMaxVersion(controlledGame.maxVersion)
+    }
+  }, [
+    controlledGame, controlledGame?.version,
+    slidingVersion, changingVersion, setSlidingVersion,
+    maxVersion, setMaxVersion
+  ])
+
+  const marks = [
+    {
+      value: 0,
+      label: '0',
+    },
+    {
+      value: maxVersion || 0,
+      label: (maxVersion || 0).toString(),
+    },
+  ];
+
+  return (
+    <Slider className={classes.versionSlider}
+      value={slidingVersion === undefined ? 0 : slidingVersion}
+      onChangeCommitted={onSlidingVersionCommit}
+      onChange={onSlidingVersionChange}
+      aria-labelledby="game-version-slider-title"
+      step={1}
+      marks={marks}
+      min={0}
+      max={maxVersion}
+      color="secondary"
+      valueLabelDisplay={ "on" }
+    />
+  )
+}
