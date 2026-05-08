34dad04c3b4b155cf72d2039f21748801e0c37b9
Nicolas Harraudeau <nharraud@users.noreply.github.com>
2026-05-08 21:40:12 +0200

    feat(css): make page reload when composed css files get changed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,40 +1,50 @@
 import path from 'path';
 
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
 import svgrPlugin from 'vite-plugin-svgr';
+import { patchCssModules } from 'vite-css-modules';
 import react from '@vitejs/plugin-react';
+import babel from "@rolldown/plugin-babel";
 
 import playfulbotConfigLoader from 'rollup-plugin-playfulbot-config-loader';
 
 // Used for intl/formatjs translations
 // https://github.com/formatjs/formatjs/issues/3225#issuecomment-1750837469
-const reactPlugin = react({
-  babel: {
-    plugins: [
-      [
-        'formatjs',
-        {
-          idInterpolationPattern: '[sha512:contenthash:base64:6]',
-          ast: true,
-        },
-      ],
-    ]
-  },
+// https://github.com/vitejs/vite-plugin-react/discussions/1148#discussioncomment-16803828
+const babelPlugin = babel({
+  plugins: [
+    [
+      'formatjs',
+      {
+        idInterpolationPattern: '[sha512:contenthash:base64:6]',
+        ast: true,
+      },
+    ],
+  ]
 });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   mode: "development",
-  plugins: [reactPlugin, reactRefresh(), svgrPlugin(), playfulbotConfigLoader()],
+  plugins: [
+    patchCssModules({ generateSourceTypes: true, declarationMap: true }),
+    react(), babelPlugin, svgrPlugin(), playfulbotConfigLoader()],
   build: {
     outDir: 'build',
+    // cssMinify: 'lightningcss',
   },
   resolve: {
     alias: {
       'src': path.resolve(__dirname, 'src'),
       'compiled-lang': path.resolve(__dirname, 'compiled-lang'),
+      '~components': path.resolve(__dirname, './src/ui/components')
     },
+  },
+  css: {
+    transformer: 'lightningcss',
+    modules: {
+      localsConvention: 'camelCaseOnly'
+    }
   },
   server: {
     host: "local-playfulbot.com",
